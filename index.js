50f9c05e3ff828363f124ebb7942c9f678e456f7
Shreyas Shinde <shindeshreyasv@gmail.com>
2024-01-28 22:40:40 +0530

    Deployed link added in URL

--- index.js
+++ index.js
@@ -4,30 +4,28 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-// const isDev = app.settings.env === "development";
-// const URL = isDev
-//   ? "http://localhost:3000"
-//   : "https://sketchbook-sigma.vercel.app";
-// app.use(cors({ origin: URL }));
+const isDev = app.settings.env === "development";
+const URL = isDev ? "http://localhost:3000" : "https://webdoodle.vercel.app/";
+app.use(cors({ origin: URL }));
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
-const io = new Server(httpServer, { cors: "http://localhost:3000" });
+const io = new Server(httpServer, { cors: URL });
 
 io.on("connection", (socket) => {
   console.log("server connected");
 
-  //   socket.on("beginPath", (arg) => {
-  //     socket.broadcast.emit("beginPath", arg);
-  //   });
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
 
-  //   socket.on("drawLine", (arg) => {
-  //     socket.broadcast.emit("drawLine", arg);
-  //   });
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
 
-  //   socket.on("changeConfig", (arg) => {
-  //     socket.broadcast.emit("changeConfig", arg);
-  //   });
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
 });
 
 httpServer.listen(5000);
